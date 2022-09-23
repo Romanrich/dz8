6256f02a759e788504db5ade49bf0f2f63a64203
Romanrich <89872537+Romanrich@users.noreply.github.com>
2022-09-24 00:28:17 +0300

    Add files via upload

---
+++ dz8.py
@@ -0,0 +1,92 @@
+from datetime import datetime, date
+import calendar
+
+for _ in range(20):
+    print()
+
+
+users = [
+    {"name": "Alex", "birthday": datetime(year=1999, month=3, day=20)},
+    {"name": "Miron", "birthday": datetime(year=1989, month=5, day=22)},
+    {"name": "Sisyastaya", "birthday": datetime(year=2002, month=9, day=23)},
+    {"name": "Zhopastaya", "birthday": datetime(year=2006, month=8, day=21)},
+    {"name": "Serhiy", "birthday": datetime(year=1996, month=2, day=22)},
+    {"name": "Leonid", "birthday": datetime(year=1996, month=4, day=20)},
+    {"name": "Davalka", "birthday": datetime(year=1996, month=6, day=22)},
+    {"name": "Roma", "birthday": datetime(year=1996, month=10, day=23)}
+]
+
+
+
+
+print("\n{:=^50}\n".format("-Birthday-"))
+
+
+
+
+
+def get_birthday_per_week(users_list):
+    current_month = date.today().month
+
+    result = {}
+
+    for person in users_list:
+
+        Month, Day = person['birthday'].month, person['birthday'].day
+        cur_year = date.today().year
+        cur_day = date.today().day
+        birthday = date(cur_year, Month, Day)
+        if Month != current_month:
+            print("***")
+            continue
+
+        print(f'My name is {person["name"]} and my birthday {Month}-{Day}')
+
+        print(f"Date of celebration - {birthday}")
+        print(f"{birthday.strftime('%A %d-%m-%y')}")
+        week_day = birthday.strftime('%A')
+        weeks_list = calendar.monthcalendar(cur_year, Month)
+
+        print()
+        for week in weeks_list:
+            if cur_day in week:
+                cur_week = week
+                ind = weeks_list.index(week)
+                pre_week = weeks_list[ind-1]
+                working_week = pre_week[-2:]+cur_week[0:-2]
+
+                if Day not in working_week:
+                    print("Not this time")
+                    continue
+
+                print(f"Сurrent week - {cur_week}")
+                print(f"Previos week - {pre_week}")
+                print(f"Working week - {working_week}")
+
+                if Day in working_week[0:2]:
+                    if 'Monday' in result:
+                        result['Monday'].append(person['name'])
+                    else:
+                        result['Monday'] = []
+                        result['Monday'].append(person['name'])
+                else:
+                    if birthday.strftime('%A') in result:
+                        result[week_day].append(person['name'])
+                    else:
+                        result[week_day] = []
+                        result[week_day].append(person['name'])
+
+                print(f"result is - {result}")
+                break
+            else:
+                continue
+        print("*"*10)
+        for day in result:
+            print(f"{day}: {', '.join(result[day])}")
+
+        return "S DR!"
+
+
+print(get_birthday_per_week(users))
+
+
